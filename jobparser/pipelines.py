98fc9f38a1d0fee60b037170d256422f87fc9d74
postas <pologrudov@gmail.com>
2019-12-24 18:13:17 -0500

    hhru spider done

---
+++ jobparser/pipelines.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from jobparser.items import JobparserItem
+from typing import Dict
+import re
+from pymongo import MongoClient
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+class JobparserPipeline(object):
+    def __init(self):
+        client = MongoClient('localhost', 27017)
+        self.db = client['scrapyDB']
+
+    def process_item(self, item:JobparserItem, spider):
+        salary = self.parse_compensation(comp=item['salary'])
+        result = {}
+        result.update(item)
+        del result['salary']
+        result.update(salary)
+        self.db[spider.name].insert_one(result)
+
+        return item
+
+    def parse_compensation(self, comp: str) -> Dict:
+        salary_from = salary_to = units = ''
+        comp_search = re.search('(от)?([0-9 ]+)?(до|-)?([0-9 ]+)? (.*)$', comp.lower().replace('\xa0', ''))
+        if comp_search:
+            if comp_search.group(2):
+                salary_from = int(comp_search.group(2))
+            if comp_search.group(3) and '-' in comp_search.group(3) or comp_search.group(
+                    3) and 'до' in comp_search.group(3):
+                salary_to = int(comp_search.group(4))
+            elif comp_search.group(1) and 'до' in comp_search.group(1):
+                salary_to = int(comp_search.group(2))
+            if salary_to or salary_from:
+                units = comp_search.group(5)
+
+        return {'salary_from': salary_from, 'salary_to': salary_to, 'units': units}
---
+++ jobparser/spiders/hhru.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.http import HtmlResponse
+from jobparser.items import JobparserItem
+
+
+class HhruSpider(scrapy.Spider):
+    name = 'hhru'
+    allowed_domains = ['hh.ru']
+    start_urls = ['https://hh.ru/search/vacancy?area=1&st=searchVacancy&text=python&page=0']
+
+    def parse(self, response:HtmlResponse):
+        next_page = response.css('a.HH-Pager-Controls-Next::attr(href)').extract_first()
+        yield response.follow(next_page, callback=self.parse)
+
+        vacancies = response.xpath('//a[@class="bloko-link HH-LinkModifier"]/@href')
+        for vacancy in vacancies:
+            yield response.follow(vacancy.extract(), callback=self.vacancy_parse)
+
+
+    def vacancy_parse(self, responce:HtmlResponse):
+        result = {}
+        result['title'] = ''.join(responce.xpath('//div[contains(@class, "vacancy-title")]/h1/span/text()').extract())
+        if not result['title']:
+            result['title'] = ''.join(responce.xpath('//div[contains(@class, "vacancy-title")]/h1/text()').extract())
+
+        result['salary'] = ''.join(responce.xpath('//p[@class="vacancy-salary"]/text()').extract())
+        result['link'] = responce.url
+        yield JobparserItem(result)
